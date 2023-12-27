d837db98e92ea821ea3dc436fde33a593b2a90d2
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2023-12-27 22:49:16 +0530

    successfully connected  Mysql

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,21 +1,34 @@
-import Hapi from '@hapi/hapi'
-import routes from './routes'
+import Hapi from "@hapi/hapi";
+import routes from "./routes";
+import { db } from "./database";
 
 const start = async () => {
-    const server = Hapi.server({
-        port: 8000,
-        host: 'localhost'
-    })
+  const server = Hapi.server({
+    port: 8000,
+    host: "localhost",
+  });
 
-    routes.forEach(route =>server.route(route));
+  routes.forEach((route) => server.route(route));
 
-    await server.start()
-    console.log('Server running on %s', server.info.uri)
-}
+  db.connection();
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
 
-process.on('uncaughtException', (err) => {
-    console.log(err)
-    process.exit(1)
-})
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+process.on("SIGINT", async () => {
+  console.log("Stopping server");
+
+  await server.stop({ timeout: 10000 });
+
+  db.end();
+
+  console.log("Server stopped");
+  process.exit(0);
+});
 
 start();
